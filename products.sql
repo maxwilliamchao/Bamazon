1dca01e6299b64188d201225bc30ba3e3265116d
maxwilliamchao <maxwilliamchao@gmail.com>
2017-12-01 08:09:41 -0800

    added products database

---
+++ products.sql
@@ -0,0 +1,10 @@
+INSERT INTO products(ProductName,DepartmentName,Price,StockQuantity)
+
+VALUES ("A", "book",50,150),
+("B", "vid", 10,60),
+("C", "book",38,100),
+("D", "book", 40, 30),
+("E", "pic", 35,75),
+("F", "pic", 50,50);
+
+SELECT * FROM bamazon.products;
