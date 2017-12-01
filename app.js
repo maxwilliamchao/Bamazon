82f4667537ad08aaae3e2bf3acc0f9b2130651ad
maxwilliamchao <maxwilliamchao@gmail.com>
2017-11-30 16:44:27 -0800

    first prompt now runs in node

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var connection = mysql.createConnection({
   user:"root",
   password:"password",
   database:"bamazon"
-})
+});
 
 connection.connect(function(err){
   if(err)throw err;
   console.log("connection!");
   makeTable();
-})
+});
 
 var makeTable = function(){
   connection.query("SELECT * FROM products", function(err, res){
@@ -25,19 +25,19 @@ var makeTable = function(){
   })
 }
 
-var promptCustomer = function(res){
+var promptCustomer = function(res) {
   inquirer.prompt([{
     type:'input',
     name:'choice',
     message:"What would you like to purchase? [Quit with Q]"
 
-  }]).then(function(answer){
+  }]).then(function(answer) {
     var correct = false;
-    if(answer.choice.toUpperCase()=="Q"){
+    if(answer.choice.toUpperCase()=="Q") {
       process.exit();
     }
-    for(var i=0;i<res.length,i++) {
-      if(res[i].productname==answer.choice){
+    for(var i=0;i<res.length;i++) {
+      if(res[i].productname==answer.choice) {
         correct=true;
         var product=answer.choice;
         var id=i;
@@ -45,17 +45,17 @@ var promptCustomer = function(res){
           type:'input',
           name:'quant',
           message:'how many do you want to buy?',
-          validate:function(value){
-            if(isNaN(value)==false){
+          validate:function(value) {
+            if(isNaN(value)==false) {
               return true;
             } else {
               return false;
             }
           }
-        }).then(function(answer){
-          if((res[id].stockquantity-answer.quant)>0){
+        }).then(function(answer) {
+          if((res[id].stockquantity-answer.quant)>0) {
             connection.query("UPDATE products SET stockquantity= '"+(res[id].stockquantity-answer.quant)
-              +" 'Where productname=' "+product+" ' ", function(err,res2){
+              +" 'Where productname=' "+product+" ' ", function(err,res2) {
                 console.log("Product Bought!");
                 makeTable();
               })
